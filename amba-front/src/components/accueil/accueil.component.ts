fef5be9214b46995f49dd27f715ce03f85b516b4
ckab04 <ciceronkapenabm@gmail.com>
2026-04-08 20:13:29 +0200

    Modifications to map the design to the web

--- amba-front/src/components/accueil/accueil.component.ts
+++ amba-front/src/components/accueil/accueil.component.ts
@@ -4,11 +4,19 @@ import { Actualites } from "./actualites/actualites.component";
 import { InfosUtiles } from "./infos-utiles/infos-utiles.component";
 import { Landing } from "./landing/landing.component";
 import { Landing1Component } from "./landing1/landing1.component";
+import { Navbar } from "../shared/navbar/navbar.component";
 
 @Component({
   selector: "app-accueil",
   standalone: true,
-  imports: [ApercuService, Actualites, InfosUtiles, Landing, Landing1Component],
+  imports: [
+    ApercuService,
+    Actualites,
+    InfosUtiles,
+    Landing,
+    Landing1Component,
+    Navbar,
+  ],
   templateUrl: "./accueil.component.html",
   styleUrl: "./accueil.component.css",
 })
