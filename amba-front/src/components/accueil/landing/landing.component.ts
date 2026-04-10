0574339124982f75385d56db668433636224f32a
ckab04 <ciceronkapenabm@gmail.com>
2026-04-10 12:27:03 +0200

    Changes on the curves of the landing page

--- amba-front/src/components/accueil/landing/landing.component.ts
+++ amba-front/src/components/accueil/landing/landing.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { Navbar } from '../../shared/navbar/navbar.component';
+import { Component } from "@angular/core";
+import { Navbar } from "../../shared/navbar/navbar.component";
 
 @Component({
-  selector: 'app-landing',
+  selector: "app-landing",
   standalone: true,
   imports: [Navbar],
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css',
+  templateUrl: "./landing.component.html",
+  styleUrl: "./landing.component.css",
 })
 export class Landing {}
